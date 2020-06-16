1adffca1fb30966e07c4492e85d041151f5721a7
everydayduffy <james.philip.duffy@gmail.com>
2020-06-16 12:29:24 +0100

    more frost

--- R/frost_degree_hours.R
+++ R/frost_degree_hours.R
@@ -1,16 +1,17 @@
-#' frost_hours: Calculate frost hours.
-#'
+#' frost_degree_hours: Calculate frost degree hours.
+#' #'
 #' @description `frost_hours` is used to calculate the number of frost hours for
 #' a given period of time.
 #'
 #' @param temps a vector of hourly temperatures.
 #' @param tme a `POSIXlt` object representing the date and time of each
 #' `temps` value.
-#' @return a single numeric value of frost hours.
+#' @return a single numeric value of frost degree hours.
 #' @export
 #'
-#' @details Frost hours are defined as temperature values that fall below 0
-#' degrees celcius.
+#' @details For each provided hourly temperature value, the number of degrees
+#' celcius below 0 are calculated. These values are then summed to provide the
+#' frost degree hours over a given period of time.
 #'
 #' @seealso the [tmecreate()] function can be used to create a POSIXlt object.
 #'
@@ -18,10 +19,9 @@
 #' tme <- tmecreate(2010, 1)
 #' plot(hourly_temps~as.POSIXct(tme), type = "l", xlab = "Month",
 #' ylab = "Temperature")
-#' frost_hours(hourly_temps, tme)
+#' frost_degree_hours(hourly_temps, tme)
 #'
-
-frost_hours <- function(temps, tme) {
+frost_degree_hours <- function(temps, tme) {
   if(as.numeric(tme[2] - as.numeric(tme[1])) != 3600) {
     stop("Temperature data are not hourly.")
   }
@@ -29,11 +29,12 @@ frost_hours <- function(temps, tme) {
     stop("Temperature and time data are differing lengths.")
   }
   if (is.na(sd(temps, na.rm = TRUE))) {
-    fh <- NA
+    fdh <- NA
   }
   else {
-    ft <- temps[temps < 0]
-    fh <- length(ft)
+    ft <- temps[which(temps < 0)]
+    ftf <- floor(ft)
+    fdh <- abs(sum(ftf))
   }
-  return(fh)
+  return(fdh)
 }
