37fb070afa8f0642737452927aab9bf5249b2510
everydayduffy <james.philip.duffy@gmail.com>
2020-06-18 16:28:08 +0100

    add length check

--- R/gst.R
+++ R/gst.R
@@ -14,13 +14,9 @@
 #' conditions (1) = yes, (0) = no accounting for temperature, precipitation and
 #' optionally daylight hours.
 #'
+#' @details `temps` must be the same length as `gseason`.
+#'
 #' @examples
-#' tme <- as.POSIXlt(c(0:1459) * 3600 * 6, origin = "2010-01-01 00:00", tz = "GMT")
-#' gs <- gseason_day(tme, 6, 21)
-#' gseason <- array(gs, dim=c(1, 1, 1460))
-#' temp <- 10 * sin(c(0:1459) / (pi * 150)) + rnorm(1460)
-#' meangst <- gst(temp, gseason)
-
 #'temps <- hourly_temps
 #'prec <- hourly_prec
 #'evap <- prec + rnorm(length(prec)) - rnorm(length(prec))
@@ -30,11 +26,12 @@
 #'tme3 <- tmecreate(2010,1)
 #'lon <- -5
 #'lat <- 52
-#'gs_bin <- gseason(temps, prec, evap, tme1, tme2, tme3, lon, lat)
-#'meangst <- gst(temps, gs_bin)
+#'gs <- gseason(temps, prec, evap, tme1, tme2, tme3, lon, lat)
+#'meangst <- gst(temps, gs)
 
-gst <- function(temps, gs_bin) {
-  tempg <- temps * gs_bin
+gst <- function(temps, gs) {
+  if(length(temps)!=length(gs)) stop("Temperature and growing season are different lengths")
+  tempg <- temps * gs
   tempg[tempg==0]<-NA
   mgst <- mean(tempg, na.rm = TRUE)
   return(mgst)
