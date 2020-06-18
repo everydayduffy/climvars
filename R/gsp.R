762fcb4679f906b5a0bbba033401dd902bc0bebb
everydayduffy <james.philip.duffy@gmail.com>
2020-06-18 16:53:46 +0100

    mean gst and total gsp

--- R/gsp.R
+++ R/gsp.R
@@ -1,20 +1,18 @@
-#' gst: Calculate mean temperature during the growing season.
+#' gsp: Calculate total precipitation during growing season.
 #'
-#' @description `gst` calculates mean temperature during the growing season.
+#' @description `gsp` calculates total precipitation during the growing season.
 #'
-#' @param temps a vector of temperature values.
+#' @param prec a vector of precipitation values.
 #' @param gs a vector of binary values indicating growing conditions
 #' (1) = yes, (0) = no.
 #'
-#' @return a single numeric value of the mean temperature during growing
+#' @return a single numeric value of the total precipitation during growing
 #' conditions.
 #' @export
 #'
 #' @seealso the [gseason()] function can be used to create an array of growing
 #' conditions (1) = yes, (0) = no accounting for temperature, precipitation and
 #' optionally daylight hours.
-#'
-#' @details `temps` must be the same length as `gseason`.
 #'
 #' @examples
 #' temps <- hourly_temps
@@ -27,12 +25,12 @@
 #' lon <- -5
 #' lat <- 52
 #' gs <- gseason(temps, prec, evap, tme1, tme2, tme3, lon, lat)
-#' meangst <- gst(temps, gs)
-
-gst <- function(temps, gs) {
-  if(length(temps)!=length(gs)) stop("Temperature and growing season vectors are different lengths")
-  tempg <- temps * gs
-  tempg[tempg==0] < -NA
-  mgst <- mean(tempg, na.rm = TRUE)
-  return(mgst)
+#' totalgsp <- gsp(prec, gs)
+#'
+gsp <- function(prec, gs) {
+  if(length(prec)!=length(gs)) stop("Precipitation and growing season vectors are different lengths")
+  precg <- prec * gs
+  precg[precg==0] <- NA
+  sgsp <- sum(precg, na.rm = TRUE)
+  return(sgsp)
 }
