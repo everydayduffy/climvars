d6b89f718a03de0f075550ae40068d773d451d2d
everydayduffy <james.philip.duffy@gmail.com>
2020-06-30 16:05:33 +0100

    docs update

--- R/data.R
+++ R/data.R
@@ -52,3 +52,30 @@
 #' @format A numeric vector of precipitation (mm/day).
 #' @source \url{https://www.ecmwf.int/en/era5-land}
 "daily_precip"
+#' Hourly potential evaporation for 2019.
+#'
+#' A dataset containing hourly potential evaporation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of potential evaporation (mm/hour).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"hourly_pev"
+#' Six-hourly potential evaporation for 2019.
+#'
+#' A dataset containing six-hourly potential evaporation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of potential evaporation (mm/six-hour).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"six_hourly_pev"
+#' Daily potential evaporation for 2019.
+#'
+#' A dataset containing daily potential evaporation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of potential evaporation (mm/day).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"daily_pev"
