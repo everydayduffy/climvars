e14a54c2a6b5fc570896c61f02428859b2ff042f
everydayduffy <james.philip.duffy@gmail.com>
2020-06-30 08:57:00 +0100

    Updating included datasets

--- R/data.R
+++ R/data.R
@@ -1,27 +1,54 @@
 #' Hourly temperatures for 2019.
 #'
-#' A dataset containing hourly 2m temperature measurements for 2019 from the
+#' A dataset containing hourly 2m temperature for 2019 from the
 #' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
 #' 57.6).
 #'
-#' @format A numeric vector.
+#' @format A numeric vector of temperatures (degrees ceclius).
 #' @source \url{https://www.ecmwf.int/en/era5-land}
 "hourly_temps"
 #' Six-hourly temperatures for 2019.
 #'
-#' A dataset containing six-hourly 2m air temperature measurements for 2019 from
+#' A dataset containing six-hourly 2m air temperature for 2019 from
 #' the ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland
 #' (-5.2, 57.6).
 #'
-#' @format A numeric vector.
+#' @format A numeric vector of temperatures (degrees ceclius).
 #' @source \url{https://www.ecmwf.int/en/era5-land}
 "six_hourly_temps"
 #' Daily temperatures for 2019.
 #'
-#' A dataset containing daily 2m air temperature measurements for 2019 from
+#' A dataset containing daily 2m air temperature for 2019 from
 #' the ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland
 #' (-5.2, 57.6).
 #'
-#' @format A numeric vector.
+#' @format A numeric vector of temperatures (degrees ceclius).
 #' @source \url{https://www.ecmwf.int/en/era5-land}
 "daily_temps"
+#' Hourly precipitation for 2019.
+#'
+#' A dataset containing hourly precipitation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of precipitation (mm/hour).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"hourly_precip"
+#' Six-hourly precipitation for 2019.
+#'
+#' A dataset containing six-hourly precipitation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of precipitation (mm/six-hour).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"six_hourly_precip"
+#' Daily precipitation for 2019.
+#'
+#' A dataset containing daily precipitation for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector of precipitation (mm/day).
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"daily_precip"
