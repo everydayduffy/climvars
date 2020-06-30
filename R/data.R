e220b7963936cabbc1135ed64a3781492f10ecb4
everydayduffy <james.philip.duffy@gmail.com>
2020-06-30 08:21:32 +0100

    Replace built in temperature data with real data

---
+++ R/data.R
@@ -0,0 +1,27 @@
+#' Hourly temperatures for 2019.
+#'
+#' A dataset containing hourly 2m temperature measurements for 2019 from the
+#' ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland (-5.2,
+#' 57.6).
+#'
+#' @format A numeric vector.
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"hourly_temps"
+#' Six-hourly temperatures for 2019.
+#'
+#' A dataset containing six-hourly 2m air temperature measurements for 2019 from
+#' the ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland
+#' (-5.2, 57.6).
+#'
+#' @format A numeric vector.
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"six_hourly_temps"
+#' Daily temperatures for 2019.
+#'
+#' A dataset containing daily 2m air temperature measurements for 2019 from
+#' the ERA-5 Land dataset. Data for a location near Kinlochewe, Scotland
+#' (-5.2, 57.6).
+#'
+#' @format A numeric vector.
+#' @source \url{https://www.ecmwf.int/en/era5-land}
+"daily_temps"
